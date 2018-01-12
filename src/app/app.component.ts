91bc1ac084e3b59a7784a0124330e053717a64ea
Vitaliy <vitaliy@altsolution.net>
2018-01-12 23:03:55 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { UsersService } from './users.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [UsersService]
 })
 export class AppComponent {
-  title = 'app';
+
+  users = []
+
+  constructor(private usersServise: UsersService) { }
+
+  ngOnInit() {
+    // this.users = this.usersServise.users
+
+    this.usersServise.getUsers().subscribe(users => {
+      // console.log(users);
+      this.users = users;
+    });
+
+  }
+
 }
---
+++ src/app/users.service.ts
@@ -0,0 +1,32 @@
+import { Http } from '@angular/http';
+import { Injectable } from '@angular/core';
+import 'rxjs/Rx'
+
+@Injectable()
+export class UsersService {
+
+    constructor(private http: Http) { }
+
+    getUsers() {
+        return this.http.get('https://randomuser.me/api/?inc=gender,name,picture,location&results=8&nat=gb')
+            .map(response => response.json())
+            .map(response => response.results)
+            .map(users => {
+                return users.map(u => {
+                    return {
+                        name: u.name.first + ' ' + u.name.last,
+                        image: u.picture.large,
+                        geo: u.location.city + ' ' + u.location.state + ' ' + u.location.street
+                    }
+                })
+            })
+    }
+
+    users = [
+        { name: 'user 1' },
+        { name: 'user 2' },
+        { name: 'user 3' },
+        { name: 'user 4' },
+        { name: 'user 5' }
+    ]
+}
